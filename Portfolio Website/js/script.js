f3ec19a36e8aaa947e2152e9af12597e048efe46
Ilkin64700 <ilkinsz@code.edu.az>
2022-10-12 01:15:32 +0400

    Final Frontend Version of Portfolio Website

--- Portfolio Website/js/script.js
+++ Portfolio Website/js/script.js
@@ -6,9 +6,25 @@ toogle.addEventListener('click', function(){
     if(this.classList.toggle('bi-brightness-high')){
         body.style.background = 'white';
         body.style.color = 'black';
+        body.style.transition = ".2s";
     }else{
         body.style.background = 'black';
         body.style.color = 'white';
-        
+        body.style.transition = ".2s";
     }
 })
+
+$('.skill-per').each(function(){
+    var $this = $(this);
+    var per = $this.attr('per');
+    $this.css("width",per+'%');
+    $({animatedValue: 0}).animate({animatedValue: per},{
+        duration: 1000,
+        step: function(){
+            $this.attr('per', Math.floor(this.animatedValue)+'%');
+        },
+        complete :function(){
+            $this.attr('per',Math.floor(this.animatedValue) +'%');
+        }
+    })
+});
